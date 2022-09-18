9f1f849eee206cac644428a902c219aedd8991cf
tocappelli <tocappelli@gmail.com>
2022-09-18 17:17:25 -0300

    entrega final

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,11 +1,13 @@
 // CREACION DE PRODUCTOS : AGREGO PRODUCTOS Y LOS ENVIA//
 class Product {
-  constructor(accountName, clientName,tableNumber, amount, productos,clarification){
+  constructor(accountName, clientName,tableNumber,bebidas,amountDrink,comida,amountFood,clarification){
    this.accountName = accountName;
    this.clientName = clientName;
    this.tableNumber = tableNumber;
-   this.amount = amount;
-   this.productos = productos;
+   this.bebidas = bebidas;
+   this.amountDrink = amountDrink;
+   this.comida = comida;
+   this.amountFood = amountFood;
    this.clarification = clarification;
    
  }
@@ -24,8 +26,10 @@ class Ui{
             <strong>Cuenta de:</strong>:${product.accountName}<br>
             <strong>Nombre del solicitante</strong>:${product.clientName}<br>
             <strong>Numero de Mesa</strong>:${product.tableNumber}<br>
-            <strong>Producto</strong>:${product.productos}<br>
-            <strong>Cantidad</strong>:${product.amount}<br>
+            <strong>Bebida</strong>:${product.bebidas}<br>
+            <strong>Cantidad</strong>:${product.amountDrink}<br>
+            <strong>Comida</strong>:${product.comida}<br>
+            <strong>Cantidad</strong>:${product.amountFood}<br>
             <strong>Aclaracion</strong>:${product.clarification}<br> 
             <button name ="close" type="button" class="btn-close text-center" aria-label="Close"></button> 
           </div>
@@ -80,17 +84,19 @@ document.getElementById ("product-form")
 .addEventListener("submit", function (e){
   const accountName = document.getElementById("accountName").value ;
   const clientName= document.getElementById("clientName").value ;
-  const amount = document.getElementById("amount").value;
   const tableNumber = document.getElementById("tableNumber").value;
-  const productos = document.getElementById("productos").value;
+  const bebidas = document.getElementById("bebidas").value;
+  const amountDrink = document.getElementById("amountDrink").value;
+  const comida = document.getElementById("comida").value;
+  const amountFood = document.getElementById("amountFood").value;
   const clarification = document.getElementById("textarea").value;
   
  
   //Productos Nuevos llamando al metodo UI (interfaz de usuario) //
    
-   const product = new Product(accountName,clientName, amount,tableNumber,productos,clarification);
-   
-    const ui = new Ui();
+  const product = new Product(accountName,clientName,tableNumber,bebidas,amountDrink,comida,amountFood,clarification);
+
+  const ui = new Ui();
 
    ui.addProduct (product);
    ui.accountNameLocal();
@@ -115,9 +121,9 @@ document.getElementById("product-list").addEventListener("click", function(e){
 
 })
 
-const tabla = document.querySelector("#productos");
+const tabla = document.querySelector("#bebidas");
 
-// FUNCION PARA CARGAR PRODUCTOS DESDE EL JASON
+// FUNCION PARA CARGAR BEBIDAS DESDE EL JSON
 function cargarProducto(){
   fetch("./Javascript/data.json") //selecciono la carpeta donde se encuentra la data json
       .then(res=>res.json()) //selecciono como quiero que se vea el json
@@ -134,6 +140,28 @@ function cargarProducto(){
       }) 
 }
 cargarProducto();
+
+// FUNCION PARA CARGAR COMIDA DESDE EL JSON
+
+const tabla2 = document.querySelector("#comida");
+
+function cargarComida(){
+  fetch("./Javascript/data2.json") //selecciono la carpeta donde se encuentra la data json
+      .then(res=>res.json()) //selecciono como quiero que se vea el json
+      // CREO UN SELECT PARA QUE EL USUARIO SELECCIONES LOS PRODUCTOS QUE SE ENCUENTRAN EN EL JSON
+      .then(listaComida=> {
+         listaComida.forEach(listaComida=> {
+          const select = document.createElement("option");
+          select.innerHTML = `
+          <option>${listaComida.name}</option>
+
+          `;
+          tabla2.appendChild(select);
+        })
+      }) 
+}
+cargarComida();
+
 
 
 
